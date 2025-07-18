c1f2090611fe1a61e430b7cc920db397b19deab9
andre-lmf <andremiyagakifilho@gmail.com>
2025-07-18 00:47:15 -0300

    Modularized main

--- alg.h
+++ alg.h
@@ -4,12 +4,20 @@
 #include <time.h>
 #include <string.h>
 
+#define RED   "\033[31m"
+#define GRN   "\033[32m"
+#define YEL   "\033[33m"
+#define BOLD  "\033[1m"
+#define RESET "\033[0m"
+
+
+
 /**
  * Função que lê uma palavra aleatória de um arquivo e a armazena na variável 'linha'
  * @param linha Ponteiro para a string onde a palavra será armazenada
  * @return void
  */
-void word(char *linha){
+void gerar_palavra(char *linha){
     srand(time(NULL));
     FILE *arquivo = fopen("./word_list/wordle-list", "r");
     if (arquivo == NULL) {
@@ -52,7 +60,7 @@ void word(char *linha){
  * @param entrada A palavra a ser procurada
  * @return true se a palavra for encontrada, false caso contrário
  */
-bool procurar_palavra(const char *entrada){
+bool procurar_palavra(char entrada[]){
     FILE *arquivo = fopen("./word_list/wordle-list", "r");
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo");
@@ -79,7 +87,7 @@ bool procurar_palavra(const char *entrada){
  * @param resultado String onde o resultado será armazenado
  * @return void
  */
-void remover_caracteres_iguais(char *str1, char*str2, char*resultado) {
+void remover_caracteres_iguais(char str1[], char str2[], char resultado[]) {
     int k = 0;
     for (int i = 0; str2[i]!='\0'; i++)
     {
@@ -105,6 +113,155 @@ void remover_caracteres_iguais(char *str1, char*str2, char*resultado) {
  * @param b Ponteiro para o segundo elemento a ser comparado
  * @return Um valor negativo se a < b, zero se a == b, ou um valor positivo se a > b
  */
-int comp(const void *a, const void *b) {
+int comparar(const void *a, const void *b) {
     return (*(char *)a - *(char *)b);  
 }
+
+/**
+ * Função que guarda as letras corretas em um array
+ * @param entrada A palavra de entrada do usuário
+ * @param palavra A palavra que o usuário está tentando adivinhar
+ * @param cores Array de cores correspondentes às letras
+ * @param palavra_var Cópia da palavra que o usuário está tentando adivinhar. _var significa que pode ser modificada sem afetar a original
+ * @param index_i Ponteiro para o índice atual do array de letras corretas
+ * @param letras_g Array onde as letras corretas serão armazenadas
+ * @return void
+ */
+void guardar_letras_corretas(char entrada[],char palavra[], char cores[], char palavra_var[], int* index_i, char letras_g[]){
+    int index = *index_i;
+    for (int i = 0; i < 5; i++)
+    {
+        if (entrada[i] == palavra[i])
+        {
+            cores[i] = 'G';
+            char *pos = strchr(palavra_var, entrada[i]);
+            if (pos)
+            {
+                *pos = ' ';
+                if (!strchr(letras_g, entrada[i]))
+                {
+                    letras_g[index++] = entrada[i];
+                    *index_i = index;
+                }
+            }
+        }
+    }
+    letras_g[strcspn(letras_g, "\n")] = '\0';
+}
+
+
+/**
+ * Função que guarda as letras parcialmente corretas em um array
+ * @param cores Array de cores correspondentes às letras
+ * @param palavra_var Cópia da palavra que o usuário está tentando adivinhar. _var significa que pode ser modificada sem afetar a original
+ * @param entrada A palavra de entrada do usuário
+ * @param letras_g Array de letras corretas
+ * @param letras_y Array de letras parcialmente corretas
+ * @param index_j Ponteiro para o índice atual do array de letras parcialmente corretas
+ * @return void
+ */
+void guardar_letras_meio_corretas(char cores[], char palavra_var[], char entrada[], char letras_g[], char letras_y[], int *index_j){
+    int index = *index_j;
+     for (int i = 0; i < 5; i++)
+        {
+            if (cores[i] != 'G' && strchr(palavra_var, entrada[i]))
+            {
+                cores[i] = 'Y';
+                char *pos = strchr(palavra_var, entrada[i]);
+                if (pos)
+                {
+                    *pos = ' ';
+                    if (!strchr(letras_y, entrada[i]) && !strchr(letras_g, entrada[i]))
+                    {
+                        letras_y[index++] = entrada[i];
+                        *index_j = index;
+                    }
+                }
+            }
+        }
+}
+
+/**
+ * Função que guarda as letras erradas em um array
+ * @param letras_e Array onde as letras erradas serão armazenadas
+ * @param entrada A palavra de entrada do usuário
+ * @param letras_y Array de letras parcialmente corretas    
+ * @param letras_g Array de letras corretas
+ * @param index_e Ponteiro para o índice atual do array de letras erradas
+ * @return void
+ */
+void guardar_letras_erradas(char letras_e[], char entrada[],char letras_y[], char letras_g[], int*index_e){
+    int index = *index_e;
+    for (int i = 0; i < 5; i++)
+        {   
+            if (!strchr(letras_e, entrada[i]) && !strchr(letras_y, entrada[i]) && !strchr(letras_g, entrada[i]))
+            {
+                letras_e[index++] = entrada[i];
+                *index_e = index;
+            }
+        }
+}
+
+/**
+ * Função que preenche um array com todas as letras do alfabeto
+ * @param letras Array onde as letras serão armazenadas
+ * @return void  
+ */
+void preencher_alfabeto(char letras[]){
+    int i = 0;
+    for (char c = 'a'; c <='z'; c++)
+    {
+        letras[i] = c;
+        i++;
+    }
+}
+
+/**
+ * Função que exibe as letras de um array
+ * @param letras Array de letras a ser exibido
+ * @return void
+ */
+void exibir_letras(char letras[]){
+    for (int i = 0; i < strlen(letras); i++)
+        {
+            if (i != strlen(letras) - 1)
+            {
+                printf("%c, ", letras[i]);
+            }
+            else
+            {
+                printf("%c", letras[i]);
+            }
+    }
+}
+
+/**
+ * Função que imprime o chute do usuário com as cores correspondentes
+ * @param cores Array de cores correspondentes às letras
+ * @param entrada A palavra de entrada do usuário
+ * @return void
+*/
+void imprimir_chute(char cores[], char entrada[]){
+    for (int i = 0; i < 5; i++)
+        {
+            if (cores[i]!='*')
+            {
+                if (cores[i]=='G')
+                {
+                    printf(BOLD);
+                    printf(GRN);
+                }
+                if(cores[i]=='Y') {
+                    printf(BOLD);
+                    printf(YEL);
+                }
+                printf("%c", entrada[i]);
+                printf(RESET);
+            } else {
+                printf(RED);
+                printf("*");
+                printf(RESET);
+            }   
+        }
+}
+
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdio.h> // printf, fgets, FILE, fopen, fclose, perror
+#include <stdbool.h> // bool
+#include <string.h> // strncpy, strcspn, strlen, strcmp
+#include <ctype.h> // tolower
+#include <stdlib.h> // srand, rand, qsort
 #include "alg.h"
 
 #define RED   "\033[31m"
@@ -17,9 +18,9 @@
  * 
  * A cada tentativa, o jogador recebe feedback sobre as letras corretas e suas posições
  * 
- * Se a letra estiver na posição correta, ela é marcada como verde (G)
- * Se a letra estiver na palavra, mas na posição errada, ela é marcada como amarela (Y)
- * Se a letra não estiver na palavra, ela não recebe marcação
+ * Se a letra estiver na posição correta, ela é marcada como verde
+ * Se a letra estiver na palavra, mas na posição errada, ela é marcada como amarela
+ * Se a letra não estiver na palavra, ela não recebe marcação (*)
  * 
  * O jogo termina quando o jogador adivinha a palavra ou esgota as tentativas
  * @param void
@@ -27,52 +28,47 @@
  */
 int main(void)
 {
-    char chute[8], palavra[8] = {0}, palavra_var[8], cores[8], letras_g[8] = {0}, letras_y[8] = {0}, letras_e[25] = {0}, letras_y_filter[8], letras[27] = {0},letras_temp[27] = {0}, letras_temp2[27] = {0}, letras_d[27] = {0};
-    int i=0;
+    char palavra[8] = {0}, palavra_var[8], cores[8], letras_g[8] = {0}, letras_y[8] = {0}, letras_e[25] = {0}, letras_y_filter[8], letras[27] = {0},letras_temp[27] = {0}, letras_temp2[27] = {0}, letras_d[27] = {0};
     /**
-     * letras_d = desconhecidas
-     * letras_e = erradas
-     * letras_g = corretas
-     * letras_y = parc. corretas
+     * palavra = palavra a ser adivinhada
+     * palavra_var = cópia de palavra usada para verificar letras
+     * cores = cores das letras do chute
+     * letras = alfabeto
+     * letras_d = letras desconhecidas
+     * letras_e = letras erradas
+     * letras_g = letras corretas
+     * letras_y = letras parc. corretas
+     * letras_y_filter = letras parc. corretas filtradas (sem letras corretas)
+     * letras_temp = letras temporárias usadas para filtrar
+     * letras_temp2 = letras temporárias usadas para filtrar
      */
-    for (char c = 'a'; c <='z'; c++)
-    {
-        letras[i] = c;
-        i++;
-    }
-    // strncpy(letras_d,letras, sizeof letras - 1);
-    // puts(letras_d);
-    bool found = false;
+    preencher_alfabeto(letras);
     int tentativas = 1, index_i, index_j, index_e;
-    word(palavra);
+    bool found = false;
+    
+    gerar_palavra(palavra);
     if(strlen(palavra) < 5) {
         printf("Erro ao ler a palavra.\n");
         return 1;
     }
-    // palavra[strcspn(palavra, "\n")] = '\0';
     strncpy(palavra_var, palavra, sizeof palavra - 1);
     char entrada[160];
     strncpy(cores, "*****", 7);
     cores[strcspn(cores, "\n")] = '\0';
-    puts(palavra); // debug
-    // strncpy(chute, "#####", 6);
-    // chute[strcspn(chute, "\n")] = '\0';
+    // puts(palavra); // debug
     for (int i = 1; i <= 6; i++)
     {
         index_i = strlen(letras_g);
         index_e = strlen(letras_e);
-        // if (i != 1)
-        // {
-        //     puts(chute);
-        // }
+        index_j = strlen(letras_y);
         printf("Chute %d: ", i);
 
         while (fgets(entrada, sizeof entrada - 1, stdin))
         {
             entrada[strcspn(entrada, "\n")] = '\0';
-            for (int i = 0; i < strlen(entrada); i++)
+            for (int j = 0; j < strlen(entrada); j++)
             {
-                entrada[i] = tolower(entrada[i]);
+                entrada[j] = tolower(entrada[j]);
             }
             if (strlen(entrada) == 5)
                 if(procurar_palavra(entrada)) {
@@ -86,153 +82,58 @@ int main(void)
             }
             printf("Chute %d: ", i);
         }
-
-
-        for (int i = 0; i < 5; i++)
-        {
-            if (entrada[i] == palavra[i])
-            {
-                chute[i] = entrada[i];
-                cores[i] = 'G';
-                char *pos = strchr(palavra_var, entrada[i]);
-                if (pos)
-                {
-                    *pos = ' ';
-                    if (!strchr(letras_g, entrada[i]))
-                    {
-                        letras_g[index_i] = entrada[i];
-                        index_i++;
-                    }
-                }
-            }
-        }
         
-        letras_g[strcspn(letras_g, "\n")] = '\0';
-        index_j = strlen(letras_y);
-        
-        for (int i = 0; i < 5; i++)
-        {
-            if (cores[i] != 'G' && strchr(palavra_var, entrada[i]))
-            {
-                cores[i] = 'Y';
-                char *pos = strchr(palavra_var, entrada[i]);
-                if (pos)
-                {
-                    *pos = ' ';
-                    if (!strchr(letras_y, entrada[i]) && !strchr(letras_g, entrada[i]))
-                    {
-                        letras_y[index_j] = entrada[i];
-                        index_j++;
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < 5; i++)
-        {   
-            if (!strchr(letras_e, entrada[i]) && !strchr(letras_y, entrada[i]) && !strchr(letras_g, entrada[i]))
-            {
-                letras_e[index_e] = entrada[i];
-                index_e++;
-            }
-        }
-        
+        guardar_letras_corretas(entrada, palavra, cores, palavra_var, &index_i, letras_g);
+        guardar_letras_meio_corretas(cores, palavra_var, entrada, letras_g, letras_y, &index_j);
+        guardar_letras_erradas(letras_e, entrada, letras_y, letras_g, &index_e);
+       
         remover_caracteres_iguais(letras_g, letras_y, letras_y_filter);
         remover_caracteres_iguais(letras_g, letras, letras_temp);
         remover_caracteres_iguais(letras_y_filter, letras_temp, letras_temp2);
         remover_caracteres_iguais(letras_e, letras_temp2, letras_d);
 
-        qsort(letras_g, strlen(letras_g), sizeof(char), comp);
-        qsort(letras_y_filter, strlen(letras_y_filter), sizeof(char), comp);
-        qsort(letras_e, strlen(letras_e), sizeof(char), comp);
+        qsort(letras_g, strlen(letras_g), sizeof(char), comparar);
+        qsort(letras_y_filter, strlen(letras_y_filter), sizeof(char), comparar);
+        qsort(letras_e, strlen(letras_e), sizeof(char), comparar);
 
         printf("Letras desconhecidas: ");
-        for (int i = 0; i < strlen(letras_d); i++)
-        {
-            if (i != strlen(letras_d) - 1)
-            {
-                printf("%c, ", letras_d[i]);
-            }
-            else
-            {
-                printf("%c", letras_d[i]);
-            }
-        }
+        exibir_letras(letras_d);
+
         printf("\n");
         
         printf(GRN);
-        printf("Letras corretas: ");
-        for (int i = 0; i < strlen(letras_g); i++)
-        {
-            if (i != strlen(letras_g) - 1)
-            {
-                printf("%c, ", letras_g[i]);
-            }
-            else
-            {
-                printf("%c", letras_g[i]);
-            }
-        }
-        printf(RESET);
 
+        printf("Letras corretas: ");
+        exibir_letras(letras_g);
+
+        printf(RESET);
 
         printf("\n");
 
         printf(YEL);
+
         printf("Letras parcialmente corretas: ");
-        for (int i = 0; i < strlen(letras_y_filter); i++)
-        {
-            if (i != strlen(letras_y_filter) - 1)
-            {
-                printf("%c, ", letras_y_filter[i]);
-            }
-            else
-            {
-                printf("%c", letras_y_filter[i]);
-            }
-        }
+        exibir_letras(letras_y_filter);
+
         printf(RESET);
+
         printf("\n");
 
         printf(RED);
+
         printf("Letras erradas: ");
-        for (int i = 0; i < strlen(letras_e); i++)
-        {
-            if (i != strlen(letras_e) - 1)
-            {
-                printf("%c, ", letras_e[i]);
-            }
-            else
-            {
-                printf("%c", letras_e[i]);
-            }
-        }
+        exibir_letras(letras_e);
+
         printf(RESET);
         
         printf("\n");
-        printf(BOLD);
-        for (int i = 0; i < 5; i++)
-        {
-            if (cores[i]!='*')
-            {
-                if (cores[i]=='G')
-                {
-                    printf(GRN);
-                } else if(cores[i]=='Y') {
-                    printf(YEL);
-                } else {
-                    printf(RESET);
-                }
-                printf("%c", entrada[i]);
-            } else {
-                printf(RESET);
-                printf("*");
-            }
-            
-            
-        }
+
+        imprimir_chute(cores, entrada);
+        
         printf(RESET);
+
         printf("\n");
-        // printf("Cores: %s\n", cores);
+
         if (!strcmp(entrada, palavra))
         {
             found = true;
